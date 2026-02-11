cc608d9382ba2f521cb4867b6bb6c4ba7fb39b8f
medlav <medlav@tuta.com>
2026-02-11 18:32:28 +0100

    updated dart docs v0.1.0

---
+++ lib/residency_portfolio.dart
@@ -0,0 +1,108 @@
+library;
+
+// L10n
+export 'l10n/app_localizations.dart';
+export 'l10n/app_localizations_en.dart';
+export 'l10n/app_localizations_it.dart';
+
+// Models
+export 'src/models/portfolio_category.dart';
+export 'src/models/portfolio_entry.dart';
+export 'src/models/profile.dart';
+export 'src/models/specialty.dart';
+
+// Providers
+export 'src/providers/category_provider.dart';
+export 'src/providers/portfolio_provider.dart';
+export 'src/providers/portfolio_search_provider.dart';
+export 'src/providers/profile_provider.dart';
+export 'src/providers/settings_provider.dart';
+
+// Screens
+export 'src/screens/activity_form_screen.dart';
+export 'src/screens/activity_list_screen.dart';
+export 'src/screens/backup_screen.dart';
+export 'src/screens/categories_screen.dart';
+export 'src/screens/category_form_screen.dart';
+export 'src/screens/curriculum_screen.dart';
+export 'src/screens/homepage_screen.dart';
+export 'src/screens/info_screen.dart';
+export 'src/screens/ongoing_activities_screen.dart';
+export 'src/screens/past_activities_screen.dart';
+export 'src/screens/portfolio_screen.dart';
+export 'src/screens/privacy_policy_screen.dart';
+export 'src/screens/profile_screen.dart';
+export 'src/screens/settings_screen.dart';
+export 'src/screens/specialties_screen.dart';
+export 'src/screens/splash_screen.dart';
+export 'src/screens/upcoming_activities_screen.dart';
+
+// Services - Main Helpers
+export 'src/services/curriculum_generator.dart';
+export 'src/services/database_helper.dart';
+export 'src/services/export_helper.dart';
+export 'src/services/import_helper.dart';
+export 'src/services/pdf_generator.dart';
+export 'src/services/specialties_data.dart';
+
+// Services - Specialties (Every single one)
+export 'src/services/specialties/allergologia.dart';
+export 'src/services/specialties/anatomia_patologica.dart';
+export 'src/services/specialties/anestesia.dart';
+export 'src/services/specialties/audiologia.dart';
+export 'src/services/specialties/cardiochirurgia.dart';
+export 'src/services/specialties/cardiologia.dart';
+export 'src/services/specialties/chirurgia_maxillofacciale.dart';
+export 'src/services/specialties/chirurgia_pediatrica.dart';
+export 'src/services/specialties/chirurgia_plastica.dart';
+export 'src/services/specialties/chirurgia_toracica.dart';
+export 'src/services/specialties/chirurgia_vascolare.dart';
+export 'src/services/specialties/chirurgia.dart';
+export 'src/services/specialties/cure_primarie.dart';
+export 'src/services/specialties/dermatologia.dart';
+export 'src/services/specialties/ematologia.dart';
+export 'src/services/specialties/endocrinologia.dart';
+export 'src/services/specialties/farmacologia.dart';
+export 'src/services/specialties/fisiatria.dart';
+export 'src/services/specialties/gastroenterologia.dart';
+export 'src/services/specialties/genetica.dart';
+export 'src/services/specialties/geriatria.dart';
+export 'src/services/specialties/ginecologia.dart';
+export 'src/services/specialties/igiene.dart';
+export 'src/services/specialties/malattie_infettive.dart';
+export 'src/services/specialties/medicina_dello_sport.dart';
+export 'src/services/specialties/medicina_di_urgenza.dart';
+export 'src/services/specialties/medicina_interna.dart';
+export 'src/services/specialties/medicina_lavoro.dart';
+export 'src/services/specialties/medicina_legale.dart';
+export 'src/services/specialties/medicina_nucleare.dart';
+export 'src/services/specialties/medicina_termale.dart';
+export 'src/services/specialties/microbiologia.dart';
+export 'src/services/specialties/nefrologia.dart';
+export 'src/services/specialties/neurochirurgia.dart';
+export 'src/services/specialties/neurologia.dart';
+export 'src/services/specialties/neuropsichiatria.dart';
+export 'src/services/specialties/oculistica.dart';
+export 'src/services/specialties/oncologia.dart';
+export 'src/services/specialties/ortopedia.dart';
+export 'src/services/specialties/otorino.dart';
+export 'src/services/specialties/patologia.dart';
+export 'src/services/specialties/pediatria.dart';
+export 'src/services/specialties/pneumologia.dart';
+export 'src/services/specialties/psichiatria.dart';
+export 'src/services/specialties/radiologia.dart';
+export 'src/services/specialties/radioterapia.dart';
+export 'src/services/specialties/reumatologia.dart';
+export 'src/services/specialties/scienze_alimentazione.dart';
+export 'src/services/specialties/specialties.dart';
+export 'src/services/specialties/statistica.dart';
+export 'src/services/specialties/urologia.dart';
+
+// Widgets
+export 'src/widgets/disclaimer_info_button.dart';
+export 'src/widgets/drawer.dart';
+export 'src/widgets/portfolio_table_simple.dart';
+export 'src/widgets/portfolio_table.dart';
+
+// Other
+export 'src/version.dart';
